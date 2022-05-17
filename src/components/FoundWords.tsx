74824da6cdb03af117098a0d8ba1463ac7b06692
Dmitry Serbin <dmitry.sbn@gmail.com>
2022-05-17 14:11:11 -0400

    Refactor FoundWords

---
+++ src/components/FoundWords.tsx
@@ -0,0 +1,28 @@
+import WordItem from './WordItem';
+
+const FoundWords = ({ foundWords }: { foundWords: string[] }) => {
+  const foundWordList = (
+    <ul>
+      {foundWords.map(word => {
+        return (
+          <WordItem
+            key={word}
+            word={word[0] + word.substring(1).toLowerCase()}
+          />
+        );
+      })}
+    </ul>
+  );
+
+  return (
+    <div className="container w-full h-full border rounded-xl">
+      <div className="m-5 mb-0">
+        You have found {foundWords.length}{' '}
+        {foundWords.length === 1 ? 'word' : 'words'}
+      </div>
+      <div className="container flex m-5">{foundWordList}</div>
+    </div>
+  );
+};
+
+export default FoundWords;
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,4 @@
-import WordItem from './WordItem';
+import FoundWords from './FoundWords';
 
 const Results = ({
   score,
@@ -7,29 +7,10 @@ const Results = ({
   score: number;
   foundWords: string[];
 }) => {
-  const foundWordList = (
-    <ul>
-      {foundWords.map(word => {
-        return (
-          <WordItem
-            key={word}
-            word={word[0] + word.substring(1).toLowerCase()}
-          />
-        );
-      })}
-    </ul>
-  );
-
   return (
     <div className="container flex flex-col">
       <div>Score: {score}</div>
-      <div className="container w-full h-full border rounded-xl">
-        <div className="m-5 mb-0">
-          You have found {foundWords.length}{' '}
-          {foundWords.length === 1 ? 'word' : 'words'}
-        </div>
-        <div className="container flex m-5">{foundWordList}</div>
-      </div>
+      <FoundWords foundWords={foundWords} />
     </div>
   );
 };
